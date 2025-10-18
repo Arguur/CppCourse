3a5df8f5653fd91834f125fc90270160d0e6fd42
Juan Francisco Huertas Coppo <150501167+Arguur@users.noreply.github.com>
2025-10-17 23:31:43 -0300

    Add files via upload

---
+++ Section5/Exercise3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+void date_of_birth() {
+    int m {};
+    int d {};
+    int y {};
+    
+    cin >> d >> y >> m;
+    
+    cout << m << " " << d << " " << y;
+    
+}
